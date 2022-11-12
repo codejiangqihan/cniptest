7527cd5b012640956d20d526f609b959daa64f6c
启涵 <qh@codeqihan.com>
2022-11-12 00:14:53 +0000

    发布了新版本3.0.1修改了错误返回
    Signed-off-by: 启涵 <qh@codeqihan.com>

--- __init__.py
+++ __init__.py
@@ -4,40 +4,40 @@ def ip():
         ip = requests.get("https://4.ipw.cn").text
         return ip
     except:
-        return "error"
+        return "Error:Unreachable"
 def ipv6():
     try:
         ip = requests.get("https://6.ipw.cn").text
         return ip
     except:
-        return "error"
+        return "Error:Unreachable"
 def test():
     try:
         ip = requests.get("https://test.ipw.cn").text
         return ip
     except:
-        return "error"
+        return "Error:Unreachable"
 def bjv4():
     try:
         ip = requests.get("https://bjv4.ipw.cn").text
         return ip
     except:
-        return "error"
+        return "Error:Unreachable"
 def bjv6():
     try:
         ip = requests.get("https://bjv6.ipw.cn").text
         return ip
     except:
-        return "error"
+        return "Error:Unreachable"
 def njv4():
     try:
         ip = requests.get("https://njv4.ipw.cn").text
         return ip
     except:
-        return "error"
+        return "Error:Unreachable"
 def njv6():
     try:
         ip = requests.get("https://njv6.ipw.cn").text
         return ip
     except:
-        return "error"
+        return "Error:Unreachable"
