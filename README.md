795de075fb018932d8e1ac5be01d8e089746cace
启涵 <qh@codeqihan.com>
2022-11-12 00:03:05 +0000

    发布了新版本，增加了北京和南京出口ipv4和ipv6查询
    Signed-off-by: 启涵 <qh@codeqihan.com>

--- README.md
+++ README.md
@@ -1,25 +1,37 @@
 # 介绍
 这是一个Python库，它可以查看当前的IP地址
-# 安装
-输入以下代码
-```
-pip install cniptest
-```
 # 获取iPv4地址
-输入以下代码
 ```
 import cniptest
 print(cniptest.ip())
 ```
 # 获取ipv6地址
-输入以下代码
 ```
 import cniptest
 print(cniptest.ipv6())
 ```
 # 检测是否有ipv6，如果有那么就返回ipv6，如果没有就返回iPv4
-输入以下代码
 ```
 import cniptest
 print(cniptest.test())
+```
+# 获取北京出口ipv4地址
+```
+import cniptest
+print(cniptest.bjv4())
+```
+# 获取北京出口ipv6地址
+```
+import cniptest
+print(cniptest.bjv6())
+```
+# 获取南京出口ipv4地址
+```
+import cniptest
+print(cniptest.njv6())
+```
+# 获取南京出口ipv4地址
+```
+import cniptest
+print(cniptest.njv6())
 ```
--- __init__.py
+++ __init__.py
@@ -1,10 +1,43 @@
 import requests
 def ip():
-    ip = requests.get("https://4.ipw.cn").text
-    return ip
+    try:
+        ip = requests.get("https://4.ipw.cn").text
+        return ip
+    except:
+        return "error"
 def ipv6():
-    ip = requests.get("https://6.ipw.cn").text
-    return ip
+    try:
+        ip = requests.get("https://6.ipw.cn").text
+        return ip
+    except:
+        return "error"
 def test():
-    ip = requests.get("https://test.ipw.cn").text
-    return ip
+    try:
+        ip = requests.get("https://test.ipw.cn").text
+        return ip
+    except:
+        return "error"
+def bjv4():
+    try:
+        ip = requests.get("https://bjv4.ipw.cn").text
+        return ip
+    except:
+        return "error"
+def bjv6():
+    try:
+        ip = requests.get("https://bjv6.ipw.cn").text
+        return ip
+    except:
+        return "error"
+def njv4():
+    try:
+        ip = requests.get("https://njv4.ipw.cn").text
+        return ip
+    except:
+        return "error"
+def njv6():
+    try:
+        ip = requests.get("https://njv6.ipw.cn").text
+        return ip
+    except:
+        return "error"
